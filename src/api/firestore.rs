0da79ef4308a9bde1c33682639d19cb264106385
bourdenas <bourdenas@gmail.com>
2023-11-10 17:16:43 +0100

    Clean-up FirestoreApi struct.

--- src/api/firestore.rs
+++ src/api/firestore.rs
@@ -4,9 +4,6 @@ use crate::Status;
 
 pub struct FirestoreApi {
     db: FirestoreDb,
-    // credentials_file: String,
-    // session: ServiceSession,
-    // next_refresh: SystemTime,
 }
 
 impl FirestoreApi {
@@ -18,33 +15,5 @@ impl FirestoreApi {
 
     pub fn db(&self) -> &FirestoreDb {
         &self.db
-    }
-
-    /// Returns a Firestore session created from input credentials.
-    // pub fn from_credentials(credentials_file: String) -> Result<Self, FirebaseError> {
-    //     let mut cred = Credentials::from_file(&credentials_file).expect("Read credentials file");
-    //     cred.download_google_jwks()
-    //         .expect("Failed to download public keys");
-
-    //     Ok(FirestoreApi {
-    //         credentials_file,
-    //         session: ServiceSession::new(cred).expect("Create a service account session"),
-    //         next_refresh: SystemTime::now()
-    //             .checked_add(Duration::from_secs(30 * 60))
-    //             .unwrap(),
-    //     })
-    // }
-
-    pub fn validate(&mut self) {
-        // if self.next_refresh <= SystemTime::now() {
-        //     let mut cred =
-        //         Credentials::from_file(&self.credentials_file).expect("Read credentials file");
-        //     cred.download_google_jwks()
-        //         .expect("Failed to download public keys");
-        //     self.session = ServiceSession::new(cred).expect("Create a service account session");
-        //     self.next_refresh = SystemTime::now()
-        //         .checked_add(Duration::from_secs(30 * 60))
-        //         .unwrap();
-        // }
     }
 }
