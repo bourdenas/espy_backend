9abdc066f532dbec695df55a367c9b1ec363041d
bourdenas <bourdenas@gmail.com>
2024-01-26 22:19:30 +0100

    Add a scraping metacritic score API.

---
+++ src/api/metacritic.rs
@@ -0,0 +1,28 @@
+use soup::prelude::*;
+
+use crate::Status;
+
+pub struct MetacriticApi {}
+
+impl MetacriticApi {
+    pub async fn get_score(slug: &str) -> Result<u64, Status> {
+        let uri = format!("https://www.metacritic.com/game/{slug}/");
+
+        let resp = reqwest::get(&uri).await?;
+        let text = resp.text().await?;
+        let soup = Soup::new(&text);
+
+        match soup.class(SCORE_TAG).find() {
+            Some(score_tag) => match score_tag.tag("span").find() {
+                Some(span) => match span.text().parse() {
+                    Ok(num) => Ok(num),
+                    Err(_) => Err(Status::not_found(format!("Missing score for {slug}"))),
+                },
+                None => Err(Status::not_found(format!("Score not found for {slug}"))),
+            },
+            None => Err(Status::not_found(format!("{slug} not found"))),
+        }
+    }
+}
+
+const SCORE_TAG: &str = "c-productScoreInfo_scoreNumber";
--- src/api/mod.rs
+++ src/api/mod.rs
@@ -2,10 +2,12 @@ mod firestore;
 mod gog;
 mod gog_token;
 mod igdb;
+mod metacritic;
 mod steam;
 
 pub use firestore::FirestoreApi;
 pub use gog::GogApi;
 pub use gog_token::GogToken;
 pub use igdb::*;
+pub use metacritic::MetacriticApi;
 pub use steam::SteamApi;
