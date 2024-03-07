5a039681712ccc952dbc458320dea95e1537ac60
bourdenas <bourdenas@gmail.com>
2024-03-07 11:51:27 +0100

    Better define the criteria for including a game in the annual review.
    This also introduces some classification for games.

--- src/batch/build_year_summary.rs
+++ src/batch/build_year_summary.rs
@@ -1,11 +1,18 @@
 use std::{
+    cmp::min,
+    collections::HashSet,
     sync::Arc,
     time::{SystemTime, UNIX_EPOCH},
 };
 
-use chrono::NaiveDateTime;
+use chrono::Utc;
 use clap::Parser;
-use espy_backend::{api::FirestoreApi, documents::*, library::firestore::year, *};
+use espy_backend::{
+    api::FirestoreApi,
+    documents::*,
+    library::firestore::{timeline, year},
+    *,
+};
 use firestore::{path, FirestoreQueryDirection, FirestoreResult};
 use futures::{stream::BoxStream, TryStreamExt};
 use itertools::Itertools;
@@ -37,11 +44,14 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
         "%Y-%m-%d %H:%M:%S",
     )?
     .timestamp();
-    let end = chrono::NaiveDateTime::parse_from_str(
-        &format!("{}-01-01 00:00:00", opts.year + 1),
-        "%Y-%m-%d %H:%M:%S",
-    )?
-    .timestamp();
+    let end = min(
+        chrono::NaiveDateTime::parse_from_str(
+            &format!("{}-01-01 00:00:00", opts.year + 1),
+            "%Y-%m-%d %H:%M:%S",
+        )?
+        .timestamp(),
+        Utc::now().naive_utc().timestamp(),
+    );
 
     let firestore = Arc::new(api::FirestoreApi::connect().await?);
 
@@ -64,54 +74,66 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
         .obj()
         .stream_query_with_errors()
         .await?;
-    let games = game_entries.try_collect::<Vec<GameEntry>>().await?;
+    let mut games = game_entries.try_collect::<Vec<GameEntry>>().await?;
+    println!("Retrieved {} titles.", games.len());
+    games.retain(|game| match game.category {
+        GameCategory::Dlc
+        | GameCategory::Bundle
+        | GameCategory::Episode
+        | GameCategory::Version
+        | GameCategory::Ignore => false,
+        _ => true,
+    });
+    println!("Retained {} titles.", games.len());
 
-    let mut i = 0;
-    let mut digests = vec![];
-    for game in games {
-        println!(
-            "#{i} -- {} -- id={} -- release={} ({})",
-            game.name,
-            game.id,
-            game.release_date,
-            NaiveDateTime::from_timestamp_millis(game.release_date * 1000).unwrap()
-        );
+    let notable = timeline::read_notable(&firestore).await?;
+    let companies = HashSet::<String>::from_iter(notable.legacy_companies.into_iter());
+    let collections = HashSet::<String>::from_iter(notable.collections.into_iter());
 
-        match game.category {
-            GameCategory::Dlc
-            | GameCategory::Bundle
-            | GameCategory::Episode
-            | GameCategory::Version
-            | GameCategory::Ignore => {
-                continue;
+    let mut partitions = games.into_iter().into_group_map_by(|game| {
+        if game.release_date == 0 {
+            match is_hyped_tbd(&game) {
+                true => "releases",
+                false => "ignore",
             }
-            _ => {}
-        }
-
-        i += 1;
-
-        digests.push(GameDigest::from(game))
-    }
-
-    let mut partitions = digests.into_iter().into_group_map_by(|digest| {
-        if digest.espy_genres.iter().any(|genre| match genre {
-            EspyGenre::Indie => digest.scores.thumbs.is_some(),
-            _ => false,
-        }) {
-            "indies"
-        } else if let GameStatus::EarlyAccess = digest.status {
-            "early_access"
-        } else if digest.scores.metacritic.is_some() || digest.scores.thumbs.is_some() {
-            "releases"
+        } else if is_early_access(&game) {
+            match is_popular_early_access(&game) {
+                true => "early_access",
+                false => "ignore",
+            }
+        } else if is_expansion(&game) && game.scores.metacritic.is_none() {
+            "expansions"
+        } else if is_indie(&game) {
+            if game.scores.metacritic.is_some() || is_popular(game) {
+                match is_casual(game) {
+                    true => "casual",
+                    false => "indies",
+                }
+            } else {
+                "ignore"
+            }
+        } else if game.scores.metacritic.is_some()
+            || is_popular(game)
+            || is_remaster(game)
+            || is_notable(game, &companies, &collections)
+            || is_gog_classic(&game)
+        {
+            match is_casual(game) {
+                true => "casual",
+                false => "releases",
+            }
         } else {
-            "debug"
+            "ignore"
         }
     });
 
     for (_, digests) in &mut partitions {
         digests.sort_by(|a, b| match b.scores.espy_score.cmp(&a.scores.espy_score) {
             std::cmp::Ordering::Equal => match b.scores.popularity.cmp(&a.scores.popularity) {
-                std::cmp::Ordering::Equal => b.scores.thumbs.cmp(&a.scores.thumbs),
+                std::cmp::Ordering::Equal => match b.scores.thumbs.cmp(&a.scores.thumbs) {
+                    std::cmp::Ordering::Equal => b.scores.hype.cmp(&a.scores.hype),
+                    other => other,
+                },
                 other => other,
             },
             other => other,
@@ -123,11 +145,50 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
             .duration_since(UNIX_EPOCH)
             .unwrap()
             .as_secs(),
-        releases: partitions.remove("releases").unwrap_or(vec![]),
-        indies: partitions.remove("indies").unwrap_or(vec![]),
-        early_access: partitions.remove("early_access").unwrap_or(vec![]),
-        debug: partitions.remove("debug").unwrap_or(vec![]),
+        releases: partitions
+            .remove("releases")
+            .unwrap_or_default()
+            .into_iter()
+            .map(|game| GameDigest::from(game))
+            .collect(),
+        indies: partitions
+            .remove("indies")
+            .unwrap_or_default()
+            .into_iter()
+            .map(|game| GameDigest::from(game))
+            .collect(),
+        expansions: partitions
+            .remove("expansions")
+            .unwrap_or_default()
+            .into_iter()
+            .map(|game| GameDigest::from(game))
+            .collect(),
+        casual: partitions
+            .remove("casual")
+            .unwrap_or_default()
+            .into_iter()
+            .map(|game| GameDigest::from(game))
+            .collect(),
+        early_access: partitions
+            .remove("early_access")
+            .unwrap_or_default()
+            .into_iter()
+            .map(|game| GameDigest::from(game))
+            .collect(),
+        debug: partitions
+            .remove("debug")
+            .unwrap_or_default()
+            .into_iter()
+            .map(|game| GameDigest::from(game))
+            .collect(),
     };
+
+    let mut i = 0;
+    for game in partitions.remove("ignore").unwrap_or_default().iter() {
+        println!("#{i} deleting {}({})", game.name, game.id);
+        i += 1;
+        library::firestore::games::delete(&firestore, game.id).await?;
+    }
 
     year::write(&firestore, &review, opts.year).await?;
 
@@ -139,6 +200,80 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     );
 
     Ok(())
+}
+
+fn is_popular(game: &GameEntry) -> bool {
+    (game.release_year() > 2011 && game.scores.popularity.unwrap_or_default() >= 10000)
+        || (game.release_year() <= 2011 && game.scores.popularity.unwrap_or_default() > 0)
+}
+
+fn is_remaster(game: &GameEntry) -> bool {
+    match game.category {
+        GameCategory::Remake | GameCategory::Remaster => true,
+        _ => false,
+    }
+}
+
+fn is_gog_classic(game: &GameEntry) -> bool {
+    game.release_year() < 2000
+        && game
+            .websites
+            .iter()
+            .any(|website| matches!(website.authority, WebsiteAuthority::Gog))
+}
+
+fn is_notable(
+    game: &GameEntry,
+    companies: &HashSet<String>,
+    collections: &HashSet<String>,
+) -> bool {
+    game.developers.iter().any(|c| companies.contains(&c.name))
+        || game
+            .collections
+            .iter()
+            .any(|c| collections.contains(&c.name))
+}
+
+fn is_casual(game: &GameEntry) -> bool {
+    game.steam_data
+        .as_ref()
+        .unwrap_or(&SteamData::default())
+        .genres
+        .iter()
+        .any(|genre| genre.description == "Casual")
+}
+
+fn is_hyped_tbd(game: &GameEntry) -> bool {
+    game.release_date == 0
+        && !matches!(game.status, GameStatus::Cancelled)
+        && game.scores.hype.unwrap_or_default() > 1
+        && game.scores.thumbs.is_none()
+        && !is_casual(&game)
+}
+
+fn is_early_access(game: &GameEntry) -> bool {
+    game.release_year() > 2018
+        && matches!(game.status, GameStatus::EarlyAccess)
+        && game.scores.metacritic.is_none()
+}
+
+fn is_popular_early_access(game: &GameEntry) -> bool {
+    game.scores.popularity.unwrap_or_default() >= 5000
+}
+
+fn is_expansion(game: &GameEntry) -> bool {
+    matches!(
+        game.category,
+        GameCategory::Expansion | GameCategory::StandaloneExpansion
+    )
+}
+
+fn is_indie(game: &GameEntry) -> bool {
+    game.release_year() > 2007
+        && game
+            .espy_genres
+            .iter()
+            .any(|genre| matches!(genre, EspyGenre::Indie))
 }
 
 #[instrument(
