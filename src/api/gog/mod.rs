513933d6b61c3d8e96365c6f9fe356e270644f4b
bourdenas <bourdenas@gmail.com>
2024-05-17 23:33:56 +0200

    Move gog apis under a common folder.

---
+++ src/api/gog/mod.rs
@@ -0,0 +1,5 @@
+mod gog;
+mod gog_token;
+
+pub use gog::GogApi;
+pub use gog_token::GogToken;
--- src/api/mod.rs
+++ src/api/mod.rs
@@ -1,14 +1,12 @@
 mod firestore;
 mod gog;
-mod gog_token;
 mod igdb;
 mod metacritic;
 mod steam;
 mod wikipedia_scrape;
 
 pub use firestore::FirestoreApi;
-pub use gog::GogApi;
-pub use gog_token::GogToken;
+pub use gog::*;
 pub use igdb::*;
 pub use metacritic::{MetacriticApi, MetacriticData};
 pub use steam::*;
