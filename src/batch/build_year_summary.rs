1164c9211dc71a2f81cc795706e91892bc162610
bourdenas <bourdenas@gmail.com>
2024-06-03 21:35:34 +0200

    Add support for running all years in the build_year_summary util.

--- src/batch/build_year_summary.rs
+++ src/batch/build_year_summary.rs
@@ -25,8 +25,8 @@ struct Opts {
     #[clap(long, default_value = "keys.json")]
     key_store: String,
 
-    #[clap(long, default_value = "2023")]
-    year: u64,
+    #[clap(long)]
+    year: Option<u64>,
 
     #[clap(long, default_value = "false")]
     cleanup: bool,
@@ -42,140 +42,153 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     let mut igdb = api::IgdbApi::new(&keys.igdb.client_id, &keys.igdb.secret);
     igdb.connect().await?;
 
-    let start = chrono::NaiveDateTime::parse_from_str(
-        &format!("{}-01-01 00:00:00", opts.year),
-        "%Y-%m-%d %H:%M:%S",
-    )?
-    .timestamp();
-    let end = min(
-        chrono::NaiveDateTime::parse_from_str(
-            &format!("{}-01-01 00:00:00", opts.year + 1),
-            "%Y-%m-%d %H:%M:%S",
-        )?
-        .timestamp(),
-        Utc::now().naive_utc().timestamp(),
-    );
-
-    let firestore = Arc::new(api::FirestoreApi::connect().await?);
-
-    let game_entries: BoxStream<FirestoreResult<GameEntry>> = firestore
-        .db()
-        .fluent()
-        .select()
-        .from("games")
-        .filter(|q| {
-            q.for_all([
-                q.field(path!(GameEntry::release_date))
-                    .greater_than_or_equal(start),
-                q.field(path!(GameEntry::release_date)).less_than(end),
-            ])
-        })
-        .order_by([(
-            path!(GameEntry::release_date),
-            FirestoreQueryDirection::Ascending,
-        )])
-        .obj()
-        .stream_query_with_errors()
-        .await?;
-    let mut games = game_entries.try_collect::<Vec<GameEntry>>().await?;
-    println!("Retrieved {} titles.", games.len());
-    games.retain(|game| match game.category {
-        GameCategory::Dlc
-        | GameCategory::Bundle
-        | GameCategory::Episode
-        | GameCategory::Version
-        | GameCategory::Ignore => false,
-        _ => true,
-    });
-    println!("Retained {} titles.", games.len());
-
-    let notable = notable::read(&firestore).await?;
-    let classifier = GameFilter::new(notable);
-
-    let mut partitions = games
-        .into_iter()
-        .into_group_map_by(|game| classifier.classify(&game));
-
-    for (_, digests) in &mut partitions {
-        digests.sort_by(|a, b| b.scores.cmp(&a.scores))
-    }
-
-    let review = AnnualReview {
-        last_updated: SystemTime::now()
-            .duration_since(UNIX_EPOCH)
-            .unwrap()
-            .as_secs(),
-        releases: partitions
-            .remove(&GameEntryClass::Main)
-            .unwrap_or_default()
-            .into_iter()
-            .map(|game| GameDigest::from(game))
-            .collect(),
-        indies: partitions
-            .remove(&GameEntryClass::Indie)
-            .unwrap_or_default()
-            .into_iter()
-            .map(|game| GameDigest::from(game))
-            .collect(),
-        remasters: partitions
-            .remove(&GameEntryClass::Remaster)
-            .unwrap_or_default()
-            .into_iter()
-            .map(|game| GameDigest::from(game))
-            .collect(),
-        expansions: partitions
-            .remove(&GameEntryClass::Expansion)
-            .unwrap_or_default()
-            .into_iter()
-            .map(|game| GameDigest::from(game))
-            .collect(),
-        casual: partitions
-            .remove(&GameEntryClass::Casual)
-            .unwrap_or_default()
-            .into_iter()
-            .map(|game| GameDigest::from(game))
-            .collect(),
-        early_access: partitions
-            .remove(&GameEntryClass::EarlyAccess)
-            .unwrap_or_default()
-            .into_iter()
-            .map(|game| GameDigest::from(game))
-            .collect(),
-        debug: partitions
-            .remove(&GameEntryClass::Debug)
-            .unwrap_or_default()
-            .into_iter()
-            .map(|game| GameDigest::from(game))
-            .collect(),
+    let start_year = match opts.year {
+        Some(year) => year,
+        None => 1979,
+    };
+    let end_year = match opts.year {
+        Some(year) => year + 1,
+        None => 2025,
     };
 
-    if opts.cleanup {
-        println!("Cleaning up the obsolete entries...");
-        let mut i = 0;
-        for game in partitions
-            .remove(&GameEntryClass::Ignore)
-            .unwrap_or_default()
-            .iter()
-        {
-            println!(
-                "#{i} deleting {}({}) -- {}",
-                game.name,
-                game.id,
-                classifier.explain(&game)
-            );
-            i += 1;
-            library::firestore::games::delete(&firestore, game.id).await?;
+    for year in start_year..end_year {
+        println!("Building library for year {year}...");
+
+        let start = chrono::NaiveDateTime::parse_from_str(
+            &format!("{}-01-01 00:00:00", year),
+            "%Y-%m-%d %H:%M:%S",
+        )?
+        .timestamp();
+        let end = min(
+            chrono::NaiveDateTime::parse_from_str(
+                &format!("{}-01-01 00:00:00", year + 1),
+                "%Y-%m-%d %H:%M:%S",
+            )?
+            .timestamp(),
+            Utc::now().naive_utc().timestamp(),
+        );
+
+        let firestore = Arc::new(api::FirestoreApi::connect().await?);
+
+        let game_entries: BoxStream<FirestoreResult<GameEntry>> = firestore
+            .db()
+            .fluent()
+            .select()
+            .from("games")
+            .filter(|q| {
+                q.for_all([
+                    q.field(path!(GameEntry::release_date))
+                        .greater_than_or_equal(start),
+                    q.field(path!(GameEntry::release_date)).less_than(end),
+                ])
+            })
+            .order_by([(
+                path!(GameEntry::release_date),
+                FirestoreQueryDirection::Ascending,
+            )])
+            .obj()
+            .stream_query_with_errors()
+            .await?;
+        let mut games = game_entries.try_collect::<Vec<GameEntry>>().await?;
+        println!("Retrieved {} titles.", games.len());
+
+        games.retain(|game| match game.category {
+            GameCategory::Dlc
+            | GameCategory::Bundle
+            | GameCategory::Episode
+            | GameCategory::Version
+            | GameCategory::Ignore => false,
+            _ => true,
+        });
+        println!("Retained {} titles.", games.len());
+
+        let notable = notable::read(&firestore).await?;
+        let classifier = GameFilter::new(notable);
+
+        let mut partitions = games
+            .into_iter()
+            .into_group_map_by(|game| classifier.classify(&game));
+
+        for (_, digests) in &mut partitions {
+            digests.sort_by(|a, b| b.scores.cmp(&a.scores))
         }
+
+        let review = AnnualReview {
+            last_updated: SystemTime::now()
+                .duration_since(UNIX_EPOCH)
+                .unwrap()
+                .as_secs(),
+            releases: partitions
+                .remove(&GameEntryClass::Main)
+                .unwrap_or_default()
+                .into_iter()
+                .map(|game| GameDigest::from(game))
+                .collect(),
+            indies: partitions
+                .remove(&GameEntryClass::Indie)
+                .unwrap_or_default()
+                .into_iter()
+                .map(|game| GameDigest::from(game))
+                .collect(),
+            remasters: partitions
+                .remove(&GameEntryClass::Remaster)
+                .unwrap_or_default()
+                .into_iter()
+                .map(|game| GameDigest::from(game))
+                .collect(),
+            expansions: partitions
+                .remove(&GameEntryClass::Expansion)
+                .unwrap_or_default()
+                .into_iter()
+                .map(|game| GameDigest::from(game))
+                .collect(),
+            casual: partitions
+                .remove(&GameEntryClass::Casual)
+                .unwrap_or_default()
+                .into_iter()
+                .map(|game| GameDigest::from(game))
+                .collect(),
+            early_access: partitions
+                .remove(&GameEntryClass::EarlyAccess)
+                .unwrap_or_default()
+                .into_iter()
+                .map(|game| GameDigest::from(game))
+                .collect(),
+            debug: partitions
+                .remove(&GameEntryClass::Debug)
+                .unwrap_or_default()
+                .into_iter()
+                .map(|game| GameDigest::from(game))
+                .collect(),
+        };
+
+        if opts.cleanup {
+            println!("Cleaning up the obsolete entries...");
+            let mut i = 0;
+            for game in partitions
+                .remove(&GameEntryClass::Ignore)
+                .unwrap_or_default()
+                .iter()
+            {
+                println!(
+                    "#{i} deleting {}({}) -- {}",
+                    game.name,
+                    game.id,
+                    classifier.explain(&game)
+                );
+                i += 1;
+                library::firestore::games::delete(&firestore, game.id).await?;
+            }
+        }
+
+        year::write(&firestore, &review, year).await?;
+
+        let serialized = serde_json::to_string(&review)?;
+        println!(
+            "Created annual review for {year} size: {}KB\n",
+            serialized.len() / 1024
+        );
     }
-
-    year::write(&firestore, &review, opts.year).await?;
-
-    let serialized = serde_json::to_string(&review)?;
-    println!(
-        "created annual review for {} size: {}KB",
-        opts.year,
-        serialized.len() / 1024
-    );
 
     Ok(())
 }
