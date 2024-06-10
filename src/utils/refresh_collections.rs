063a79115bc41909e2797099f17d0430db98c927
bourdenas <bourdenas@gmail.com>
2024-06-10 23:27:48 +0200

    Update refresh_collections util to be streaming.

--- src/utils/refresh_collections.rs
+++ src/utils/refresh_collections.rs
@@ -1,21 +1,24 @@
+use std::{
+    sync::Arc,
+    time::{SystemTime, UNIX_EPOCH},
+};
+
 use clap::Parser;
 use espy_backend::{
     api::FirestoreApi,
-    documents::{Collection, GameDigest},
-    *,
+    documents::{Collection, GameCategory, GameDigest},
+    library, Tracing,
 };
-use tracing::{error, info, instrument};
+use firestore::{struct_path::path, FirestoreQueryDirection, FirestoreResult};
+use futures::{stream::BoxStream, StreamExt};
+use itertools::Itertools;
+use tracing::{error, warn};
 
 /// Espy util for refreshing IGDB and Steam data for GameEntries.
 #[derive(Parser)]
 struct Opts {
-    /// Refresh collection with specified id.
-    #[clap(long)]
-    id: Option<u64>,
-
-    /// If set, delete game entry instead of refreshing it.
-    #[clap(long)]
-    delete: bool,
+    #[clap(long, default_value = "0")]
+    cursor: u64,
 }
 
 #[tokio::main]
@@ -23,56 +26,83 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     Tracing::setup("utils/refresh_collections")?;
 
     let opts: Opts = Opts::parse();
+    let mut cursor = opts.cursor;
 
-    let firestore = api::FirestoreApi::connect().await?;
+    let mut i = 0;
+    while i % BATCH_SIZE == 0 {
+        let firestore = Arc::new(FirestoreApi::connect().await?);
 
-    if let Some(id) = opts.id {
-        match opts.delete {
-            false => refresh_collection(firestore, id).await?,
-            true => library::firestore::collections::delete(&firestore, id).await?,
-        }
-    } else {
-        refresh_collections(firestore).await?;
-    }
+        let mut collections: BoxStream<FirestoreResult<Collection>> = firestore
+            .db()
+            .fluent()
+            .select()
+            .from("collections")
+            .filter(|q| q.for_all([q.field(path!(Collection::id)).greater_than_or_equal(cursor)]))
+            .order_by([(path!(Collection::id), FirestoreQueryDirection::Ascending)])
+            .limit(BATCH_SIZE)
+            .obj()
+            .stream_query_with_errors()
+            .await?;
 
-    Ok(())
-}
+        while let Some(collection) = collections.next().await {
+            match collection {
+                Ok(collection) => {
+                    cursor = collection.id;
 
-async fn refresh_collection(firestore: FirestoreApi, id: u64) -> Result<(), Status> {
-    let collection = library::firestore::collections::read(&firestore, id).await?;
-    refresh(firestore, vec![collection]).await
-}
+                    println!(
+                        "#{i} -- {} -- id={} -- {} main games ({} total)",
+                        collection.name,
+                        collection.id,
+                        collection.games.iter().fold(0, |acc, e| acc
+                            + match e.category {
+                                GameCategory::Main => 1,
+                                _ => 0,
+                            }),
+                        collection.games.len()
+                    );
 
-#[instrument(level = "trace", skip(firestore))]
-async fn refresh_collections(firestore: FirestoreApi) -> Result<(), Status> {
-    let collections = library::firestore::collections::list(&firestore).await?;
-    refresh(firestore, collections).await
-}
+                    let start = SystemTime::now()
+                        .duration_since(UNIX_EPOCH)
+                        .unwrap()
+                        .as_millis();
 
-async fn refresh(firestore: FirestoreApi, collections: Vec<Collection>) -> Result<(), Status> {
-    info!("Updating {} collections...", collections.len());
+                    let (games, missing) = library::firestore::games::batch_read(
+                        &firestore,
+                        &collection.games.iter().map(|e| e.id).collect_vec(),
+                    )
+                    .await?;
 
-    for collection in collections {
-        info!("updating {}", &collection.slug);
+                    if !missing.is_empty() {
+                        warn!(
+                            "missing {} GameEntries from collection '{}' ({})",
+                            missing.len(),
+                            &collection.name,
+                            collection.id,
+                        );
+                    }
 
-        let mut game_digest: Vec<GameDigest> = vec![];
-        for game in collection.games {
-            if let Ok(game_entry) = library::firestore::games::read(&firestore, game.id).await {
-                game_digest.push(GameDigest::from(game_entry))
+                    let collection = Collection {
+                        id: collection.id,
+                        name: collection.name,
+                        slug: collection.slug,
+                        url: collection.url,
+                        games: games.into_iter().map(|e| GameDigest::from(e)).collect_vec(),
+                    };
+                    library::firestore::collections::write(&firestore, &collection).await?;
+
+                    let finish = SystemTime::now()
+                        .duration_since(UNIX_EPOCH)
+                        .unwrap()
+                        .as_millis();
+                    println!("  -- {} msec", finish - start);
+                }
+                Err(status) => error!("{status}"),
             }
-        }
-
-        let collection = Collection {
-            id: collection.id,
-            name: collection.name,
-            slug: collection.slug,
-            url: collection.url,
-            games: game_digest,
-        };
-        if let Err(e) = library::firestore::collections::write(&firestore, &collection).await {
-            error!("{e}");
+            i += 1;
         }
     }
 
     Ok(())
 }
+
+const BATCH_SIZE: u32 = 1000;
