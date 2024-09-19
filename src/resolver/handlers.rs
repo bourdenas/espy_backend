c24e3eb6e26e39337012a28c8b1e3da5e0a8fafb
bourdenas <bourdenas@gmail.com>
2024-09-19 11:14:20 +0200

    Implement the retrieve and digest API calls.

--- src/resolver/handlers.rs
+++ src/resolver/handlers.rs
@@ -12,8 +12,22 @@ pub async fn post_retrieve(
     id: u64,
     firestore: Arc<FirestoreApi>,
     igdb: Arc<IgdbApi>,
-) -> Result<impl warp::Reply, Infallible> {
-    Ok(StatusCode::OK)
+) -> Result<Box<dyn warp::Reply>, Infallible> {
+    match igdb.get(id).await {
+        Ok(igdb_game) => match igdb.resolve_only(firestore, igdb_game).await {
+            Ok(game_entry) => Ok(Box::new(warp::reply::json(&game_entry))),
+            Err(Status::NotFound(_)) => Ok(Box::new(StatusCode::NOT_FOUND)),
+            Err(status) => {
+                error!("post_digest: {status}");
+                Ok(Box::new(StatusCode::INTERNAL_SERVER_ERROR))
+            }
+        },
+        Err(Status::NotFound(_)) => Ok(Box::new(StatusCode::NOT_FOUND)),
+        Err(status) => {
+            error!("post_digest: {status}");
+            Ok(Box::new(StatusCode::INTERNAL_SERVER_ERROR))
+        }
+    }
 }
 
 #[instrument(level = "trace", skip(igdb_game, firestore, igdb,))]
@@ -22,7 +36,7 @@ pub async fn post_resolve(
     firestore: Arc<FirestoreApi>,
     igdb: Arc<IgdbApi>,
 ) -> Result<Box<dyn warp::Reply>, Infallible> {
-    match igdb.resolve_only(Arc::clone(&firestore), igdb_game).await {
+    match igdb.resolve_only(firestore, igdb_game).await {
         Ok(game_entry) => Ok(Box::new(warp::reply::json(&game_entry))),
         Err(Status::NotFound(_)) => Ok(Box::new(StatusCode::NOT_FOUND)),
         Err(status) => {
@@ -37,6 +51,20 @@ pub async fn post_digest(
     id: u64,
     firestore: Arc<FirestoreApi>,
     igdb: Arc<IgdbApi>,
-) -> Result<impl warp::Reply, Infallible> {
-    Ok(StatusCode::OK)
+) -> Result<Box<dyn warp::Reply>, Infallible> {
+    match igdb.get(id).await {
+        Ok(igdb_game) => match igdb.resolve_digest(&firestore, igdb_game).await {
+            Ok(digest) => Ok(Box::new(warp::reply::json(&digest))),
+            Err(Status::NotFound(_)) => Ok(Box::new(StatusCode::NOT_FOUND)),
+            Err(status) => {
+                error!("post_digest: {status}");
+                Ok(Box::new(StatusCode::INTERNAL_SERVER_ERROR))
+            }
+        },
+        Err(Status::NotFound(_)) => Ok(Box::new(StatusCode::NOT_FOUND)),
+        Err(status) => {
+            error!("post_digest: {status}");
+            Ok(Box::new(StatusCode::INTERNAL_SERVER_ERROR))
+        }
+    }
 }
