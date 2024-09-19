33c0e4b49ff2384a762ad87b7e7d6fe3b2d1fb5d
bourdenas <bourdenas@gmail.com>
2024-09-19 23:30:53 +0200

    Simplify refresh_library_entries to only update from existing GameEntries.

--- src/utils/refresh_library_entries.rs
+++ src/utils/refresh_library_entries.rs
@@ -1,27 +1,21 @@
-use std::{collections::HashMap, sync::Arc};
+use std::collections::HashMap;
 
 use clap::Parser;
 use csv::Writer;
 use espy_backend::{
-    api::{FirestoreApi, IgdbApi},
+    api::FirestoreApi,
     documents::{GameCategory, GameDigest, Library, LibraryEntry},
-    library, util, Status, Tracing,
+    library, Status, Tracing,
 };
+use itertools::Itertools;
 use serde::{Deserialize, Serialize};
-use tracing::{error, info, instrument};
+use tracing::{info, instrument};
 
 /// Espy util for refreshing IGDB and Steam data for GameEntries.
 #[derive(Parser)]
 struct Opts {
     #[clap(long)]
     user: String,
-
-    /// JSON file that contains application keys for espy service.
-    #[clap(long, default_value = "keys.json")]
-    key_store: String,
-
-    #[clap(long)]
-    resolve: bool,
 
     /// Export in a text file the library (for inspection) instead of refreshing it.
     #[clap(long, default_value = "")]
@@ -39,10 +33,6 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     let opts: Opts = Opts::parse();
     let firestore = FirestoreApi::connect().await?;
 
-    let keys = util::keys::Keys::from_file(&opts.key_store).unwrap();
-    let mut igdb = IgdbApi::new(&keys.igdb.client_id, &keys.igdb.secret);
-    igdb.connect().await?;
-
     if !opts.export_csv.is_empty() {
         let library = library::firestore::library::read(&firestore, &opts.user).await?;
         export_library(library, &opts.export_csv)?;
@@ -51,87 +41,50 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
         let text = summary_library(library);
         println!("{text}");
     } else {
-        refresh_library_entries(firestore, igdb, &opts.user, opts.resolve).await?;
+        refresh_library_entries(firestore, &opts.user).await?;
     }
 
     Ok(())
 }
 
-#[instrument(level = "trace", skip(firestore, igdb, user_id))]
-async fn refresh_library_entries(
-    firestore: FirestoreApi,
-    igdb: IgdbApi,
-    user_id: &str,
-    resolve: bool,
-) -> Result<(), Status> {
-    let legacy_library = library::firestore::library::read(&firestore, user_id).await?;
-    info!("updating {} titles...", legacy_library.entries.len());
+#[instrument(level = "trace", skip(firestore, user_id))]
+async fn refresh_library_entries(firestore: FirestoreApi, user_id: &str) -> Result<(), Status> {
+    let library = library::firestore::library::read(&firestore, user_id).await?;
+    info!("updating {} titles...", library.entries.len());
 
-    let firestore = Arc::new(firestore);
+    let result = library::firestore::games::batch_read(
+        &firestore,
+        &library.entries.iter().map(|e| e.id).collect_vec(),
+    )
+    .await?;
 
-    let mut library_entries: HashMap<u64, LibraryEntry> = HashMap::new();
-    let mut k = 0;
-    for mut entry in legacy_library.entries {
-        println!(
-            "#{k} Get '{title}' ({id})",
-            title = entry.digest.name,
-            id = entry.id,
+    if !result.not_found.is_empty() {
+        panic!(
+            "Entries in library were not found in `/games` collection: {:?}",
+            result.not_found
         );
-
-        let game_entry = if resolve {
-            let igdb_game = igdb.get(entry.id).await?;
-            igdb.resolve(Arc::clone(&firestore), igdb_game).await
-        } else {
-            library::firestore::games::read(&firestore, entry.id).await
-        };
-
-        let digest = match game_entry {
-            Ok(game_entry) => GameDigest::from(game_entry),
-            Err(_) => match igdb.get(entry.id).await {
-                Ok(igdb_game) => {
-                    info!("#{k} Fetching from igdb '{title}'", title = igdb_game.name);
-                    match igdb.resolve_digest(&firestore, igdb_game).await {
-                        Ok(game_entry) => game_entry,
-                        Err(e) => {
-                            error!("Failed to igdb.get_digest: {e}");
-                            continue;
-                        }
-                    }
-                }
-                Err(e) => {
-                    error!("Failed to igdb.get {}: {e}", entry.id);
-                    continue;
-                }
-            },
-        };
-
-        entry.digest = digest;
-        library_entries
-            .entry(entry.id)
-            .and_modify(|e| {
-                e.store_entries
-                    .extend(entry.store_entries.iter().map(|e| e.clone()))
-            })
-            .or_insert(entry);
-        k += 1;
     }
 
-    let library = Library {
-        entries: library_entries
-            .into_iter()
-            .map(|(_, mut entry)| {
-                entry.store_entries.sort_by(|a, b| match a.id.cmp(&b.id) {
-                    std::cmp::Ordering::Equal => a.storefront_name.cmp(&b.storefront_name),
-                    e => e,
-                });
-                entry
-                    .store_entries
-                    .dedup_by(|a, b| a.id == b.id && a.storefront_name == b.storefront_name);
-                entry
-            })
-            .collect(),
-    };
+    let library_entries: HashMap<u64, LibraryEntry> =
+        HashMap::from_iter(library.entries.into_iter().map(|e| (e.id, e)));
 
+    let entries = result
+        .documents
+        .into_iter()
+        .map(|game_entry| {
+            let library_entry = library_entries
+                .get(&game_entry.id)
+                .expect("Retrieved a game entry that is not present in the user library.");
+            LibraryEntry {
+                id: game_entry.id,
+                digest: GameDigest::from(game_entry),
+                store_entries: library_entry.store_entries.clone(),
+                added_date: library_entry.added_date,
+            }
+        })
+        .collect_vec();
+
+    let library = Library { entries };
     let serialized = serde_json::to_string(&library)?;
     info!("updated library size: {}KB", serialized.len() / 1024);
     library::firestore::library::write(&firestore, user_id, library).await?;
