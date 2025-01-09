6d7c73997529c4f1826eb29706350ea1b34b9362
bourdenas <bourdenas@gmail.com>
2025-01-09 22:31:19 +0100

    Add a steam_update batch job.

---
+++ src/batch/steam_update.rs
@@ -0,0 +1,73 @@
+use clap::Parser;
+use espy_backend::{
+    api::{FirestoreApi, SteamDataApi, SteamScrape},
+    documents::GameEntry,
+    library, stream_games, Status,
+};
+use firestore::{path, FirestoreQueryDirection};
+
+/// Espy util for refreshing IGDB and Steam data for GameEntries.
+#[derive(Parser)]
+struct Opts {
+    #[clap(long, default_value = "2015")]
+    start_year: u64,
+
+    #[clap(long, default_value = "false")]
+    cleanup: bool,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    // Tracing::setup("batch/steam_update")?;
+
+    let opts: Opts = Opts::parse();
+
+    let start = chrono::DateTime::parse_from_str(
+        &format!("{}-01-01 00:00:00 +0000", opts.start_year),
+        "%Y-%m-%d %H:%M:%S %z",
+    )
+    .expect("Failed to parse start date")
+    .timestamp();
+
+    stream_games!(
+        batch: 400,
+        filter: |q| {
+            q.for_all([
+                q.field(path!(GameEntry::steam_appid)).is_not_null(),
+                q.for_any([
+                    q.field(path!(GameEntry::release_date))
+                        .greater_than_or_equal(start),
+                    q.field(path!(GameEntry::release_date)).equal(0),
+                ]),
+            ])
+        },
+        ordering: [(
+            path!(GameEntry::release_date),
+            FirestoreQueryDirection::Ascending,
+        )],
+        retrieve_steam
+    );
+
+    Ok(())
+}
+
+async fn retrieve_steam(
+    firestore: &FirestoreApi,
+    game_entry: &mut GameEntry,
+) -> Result<(), Status> {
+    let steam_appid = format!("{}", game_entry.steam_appid.unwrap());
+
+    let steam = SteamDataApi::new();
+    let steam_data = steam.retrieve_steam_data(&steam_appid).await?;
+    game_entry.add_steam_data(steam_data);
+
+    let website = format!("https://store.steampowered.com/app/{steam_appid}/");
+    let scraped_data = SteamScrape::scrape(&website).await?;
+    if let Some(steam_data) = &mut game_entry.steam_data {
+        steam_data.user_tags = scraped_data.user_tags;
+    }
+
+    library::firestore::games::write(firestore, game_entry).await?;
+
+    Ok(())
+}
